cd375edaf415ce868d04ab1a942ac29a254257ec
Shelley <shelley.lally@gmail.com>
2020-04-04 13:42:35 +0100

    section 4 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Pipe } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +7,20 @@ import { CourseComponent } from './course/course.component';
 import { CoursesService } from './course/course.service';
 import { AuthorsComponent } from './authors/authors.component';
 import { AuthorsService } from './authors.service';
+import { FormsModule } from '@angular/forms';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
     AuthorsComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [
     AuthorsService
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -4,11 +4,60 @@ import { CoursesService } from './course.service'
 @Component({
   selector: 'courses',
   template: `
-      <button class="btn btn-primary">Save</button>
-  `
+      <!-- adding bootstrap -->
+      <button class="btn btn-primary" [class.active]="isActive">Save</button>
+      
+      <!-- class binding --> <br><br>
+      <button class="btn btn-primary" [style.backgroundColor]="isActive ? 'blue' : 'red'">Background Colour</button>
+      
+      <!-- event binding --> <br><br>
+      <button  class="btn btn-primary" (click)="onClick($event)">Click</button>
+      
+      <!-- template variable --> <br><br>
+      <input #email (keyup.enter)="onKeyUpForTemplateVariable(email.value)"/>
+
+      <!-- two-way binding --> <br><br>
+      <input [(ngModel)]="emailAddress" (keyup.enter)="onKeyUp()"/>
+
+      <!-- pipes --> <br><br>
+      {{ course.title | uppercase}} <br>
+      {{ course.students | number}} <br>
+      {{ course.rating | number: '1.1-1' }} <br>
+      {{ course.price | currency:'EUR' }} <br>
+      {{ course.releaseDate | date:'shortDate'}} <br>
+
+      <!-- custom pipes --> <br><br>
+      {{ text | summary:10 }}
+
+      `
 })
 export class CourseComponent implements OnInit {
 
+  isActive = false;
+
+  emailAddress="shelley.lally@gmail.com";
+
+  course = {
+    title: "The Complete Angular Course",
+    rating: 4.956,
+    students: 20543,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
+  };
+
+  text="LytPzuLuFxVkhBSQHscygKtlC8QcRrex5k6OzuDFAVndOthmoAu4eGbUVSeXDvxLYZgdSjAMLnBF2cKfv2zwS4Bhf1syoLBLIZYqUNUBdSqnyXDlHBGnJr3nH1fS1cK7AqeWawYpB4jXOWMXxgp21QyF1cR6pcl2Aw4nzlW63ennJYY5rcgImMDfA2hk5MwPof46Y2yw";
+
+  onKeyUp() {
+      console.log(this.emailAddress);
+      }
+
+  onKeyUpForTemplateVariable(email) {
+      console.log(email);
+  }
+
+  onClick($event){
+    console.log("Button was clicked",  $event);
+  }
 
   ngOnInit(): void {
   }
