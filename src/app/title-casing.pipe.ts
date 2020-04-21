3ccbe350e1a60f07f6e63c46aef88801c2765a4e
Shelley <shelley.lally@gmail.com>
2020-04-21 15:18:21 +0100

    pipe updates

--- src/app/title-casing.pipe.ts
+++ src/app/title-casing.pipe.ts
@@ -7,15 +7,30 @@ export class TitleCasingPipe implements PipeTransform {
 
     transform(value: string, args?: any): string {
         if (!value) return value;
-        if (typeof value !== 'string') {
-          throw Error("Not a String");
+
+        let words = value.split(' ');
+        for (var i = 0; i < words.length; i++) {
+            if (i > 0 && this.isPreposition(words[i]))
+                words[i] = words[i].toLowerCase();
+            else {
+                words[i] = this.toTitleCase(words[i]);
+            }
         }
 
-        if (value.substr(1) != "the" || value.substr(1) != "of")
-           return value[0].toUpperCase() + value.substr(1).toLowerCase();
-        else
-            return value;
-      
+        return words.join(' '); 
     }
 
+    private isPreposition(word:string): boolean {
+        let prepositions = [
+            'of', 'the', 'and'
+        ];
+
+        return prepositions.includes(word.toLowerCase());
+         
+    }
+
+    private toTitleCase(word:string): string {
+        return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
+
+    }
 }
