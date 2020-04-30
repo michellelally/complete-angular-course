c68b02714d7e659b5294fe24297d10c2128931d9
Shelley <shelley.lally@gmail.com>
2020-04-30 22:09:18 +0100

    implementing ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,7 @@ import { FavouriteChangeEventArgs } from './favourite/favourite.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  courses;
 
-  loadCourses() {
-    this.courses = [
-      { id: 1, name: 'course1' },
-      { id: 2, name: 'course2' },
-      { id: 3, name: 'course3' },
-    ];
-  }
-
-  // check if course object exists and return its id otherwise return undefined
-  // changes the way angular searches for objects from object identity to course id
-  trackCourse(index, course) {
-    return course ? course.id : undefined;
-  }
+  canSave = false;
 
 }
