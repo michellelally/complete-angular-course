3799ee0bb3129c61bbecf0bcc0b8af82a72a82e0
Shelley <shelley.lally@gmail.com>
2020-04-30 20:16:42 +0100

    ngFor and loading using trackBy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,20 @@ import { FavouriteChangeEventArgs } from './favourite/favourite.component';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-  ];
+  courses;
 
-  onAdd(){
-    this.courses.push({ id: 4, name: 'course4' });
+  loadCourses() {
+    this.courses = [
+      { id: 1, name: 'course1' },
+      { id: 2, name: 'course2' },
+      { id: 3, name: 'course3' },
+    ];
   }
 
-  onRemove(course){
-    let index = this.courses.indexOf(course);
-    this.courses.splice(index, 1);
-  }
-
-
-  onChange(course){
-    course.name = "updated";
+  // check if course object exists and return its id otherwise return undefined
+  // changes the way angular searches for objects from object identity to course id
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
   }
 
 }
