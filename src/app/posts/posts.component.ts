be329fb0d6c57fdf8c18a9953d4511b3fd6b3885
Shelley <shelley.lally@gmail.com>
2020-05-13 17:27:16 +0100

    CRUD operations implemented and working

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -31,5 +31,28 @@ export class PostsComponent  {
     })
   }
 
+  updatePost(post){
+    // put is to send the whole object to the server 
+    //this.http.put(this.url, JSON.stringify(post))
+    // patch is used to send a few properties of an object to server
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response.json());
+      })
+
+  }
+
+  deletePost(post){
+    // put is to send the whole object to the server 
+    //this.http.put(this.url, JSON.stringify(post))
+    // patch is used to send a few properties of an object to server
+    this.http.delete(this.url + '/' + post.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+      })
+
+  }
+
 
 }
