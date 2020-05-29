5c00744c4eada8ba32903288b3d3beff5fa8f956
Shelley <shelley.lally@gmail.com>
2020-05-29 17:08:37 +0100

    combining multiple observables

--- src/app/followers/followers.component.ts
+++ src/app/followers/followers.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../services/users.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, observable } from 'rxjs';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-followers',
@@ -10,11 +13,26 @@ export class FollowersComponent implements OnInit {
 
   users: any[];
 
-  constructor(private service: UsersService) { }
+  constructor(private service: UsersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+
     this.service.getAll().subscribe(
       users => this.users = users);
+
+    this.route.paramMap
+      .subscribe(params => {
+
+      });
+
+    this.route.queryParamMap
+      .subscribe(params => {
+
+      });
   }
 
 
